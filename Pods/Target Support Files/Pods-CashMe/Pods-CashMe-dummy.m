45203cc6ddab5fb0be604a22c7a7593b21739606
Adrian McClure <adrianmcclure@adrians-macbook-pro.local>
2016-04-16 18:51:05 -0500

    Removed pods

--- Pods/Target Support Files/Pods-CashMe/Pods-CashMe-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CashMe : NSObject
-@end
-@implementation PodsDummy_Pods_CashMe
-@end
--- Pods/Target Support Files/Pods-CashMe/Pods-CashMe-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CashMeVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CashMeVersionString[];
-
